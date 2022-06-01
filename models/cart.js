c849783cb50306e8d5cfd4e6a6ac348e4a5c34ab
cedric karungu <ckarungu921@gmail.com>
2022-06-02 01:07:48 +0200

    fix: fix missing on deleting product

--- models/cart.js
+++ models/cart.js
@@ -44,7 +44,10 @@ module.exports = class Cart {
 				return;
 			}
 			const updatedCart = { ...JSON.parse(fileContent) };
-			const product = updatedCart.products.find((prod) => prod.id === id);
+            const product = updatedCart.products.find((prod) => prod.id === id);
+            if (!product) {
+                return;
+            }
 			const productQty = product ? product.qty : 0;
 			updatedCart.products = updatedCart.products.filter(
 				(prod) => prod.id !== id
