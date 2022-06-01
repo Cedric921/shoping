453009f40aa312fd7614ef837eaf1c04c4da05ac
cedric karungu <ckarungu921@gmail.com>
2022-06-01 14:14:13 +0200

    feature: add cart logic

--- models/cart.js
+++ models/cart.js
@@ -9,7 +9,25 @@ const p = path.join(
 
 
 module.exports = class Cart {
-    static addProduct(id) {
-        
+    static addProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 };
+            if (!err) {
+                cart = JSON.parse(fileContent);
+            }
+
+            //verify if the product exist on the cart
+            const existingProduct = cart.products.find(prod => prod.id === id);
+            let updatedProduct;
+            if (existingProduct) {
+                updatedProduct = { ...existingProduct };
+                updatedProduct.qty = updatedProduct.qty + 1;
+            }
+            else {
+                updatedProduct = { id: id, qty: 1 };
+            }
+            cart.totalPrice = cart.totalPrice + productPrice;
+        });
+        //add a new product / increase quantity
     }
 }
